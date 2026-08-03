b030e8d80a5e402aa33be0db0e2d2a8e7225db80
kumaraguru-wq <kumaraguru-wq@users.noreply.github.com>
2026-08-03 15:38:09 +0530

    Match PWA chrome to dark theme

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'chatfind-shell-v7'
+const CACHE_NAME = 'chatfind-shell-v8'
 const APP_SHELL = ['./', './index.html', './manifest.webmanifest', './icon.svg']
 
 self.addEventListener('install', (event) => {
